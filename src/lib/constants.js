89f28d4085ba381f2023c5bcc209f5c46bec052b
Cosmas-ogo <anene.ogochukwu@yahoo.com>
2025-10-01 12:48:02 +0100

    feat(lib): add application constants for app name and preset search terms

---
+++ src/lib/constants.js
@@ -0,0 +1,12 @@
+export const APP_NAME = "Exhibition Curator";
+
+export const PRESET_TERMS = [
+  "Impressionism",
+  "Egypt",
+  "Ceramics",
+  "Portrait",
+  "Swords",
+  "Nature",
+  "Textiles",
+  "Buddhist",
+];
